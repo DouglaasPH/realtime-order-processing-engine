42f2fca56a9d6e2573d896dcaf0b40d8ca992892
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-09-24 18:07:28 -0300

    add representational layer

--- pedidos/src/main/java/io/github/douglaasph/icompras/pedidos/model/Pedido.java
+++ pedidos/src/main/java/io/github/douglaasph/icompras/pedidos/model/Pedido.java
@@ -1,5 +1,6 @@
 package io.github.douglaasph.icompras.pedidos.model;
 
+import io.github.douglaasph.icompras.pedidos.client.representation.ClienteRepresentation;
 import io.github.douglaasph.icompras.pedidos.model.enums.StatusPedido;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -48,4 +49,7 @@ public class Pedido {
 
     @OneToMany(mappedBy = "pedido")
     private List<ItemPedido> itens;
+
+    @Transient
+    private ClienteRepresentation dadosCliente;
 }
