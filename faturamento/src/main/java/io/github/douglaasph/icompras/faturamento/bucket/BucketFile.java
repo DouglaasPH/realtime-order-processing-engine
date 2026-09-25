249673239235fc5f1bf3a7407d93940709bb37b2
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-09-25 16:08:08 -0300

    add config for bucket and two endpoints of upload and get-url

---
+++ faturamento/src/main/java/io/github/douglaasph/icompras/faturamento/bucket/BucketFile.java
@@ -0,0 +1,8 @@
+package io.github.douglaasph.icompras.faturamento.bucket;
+
+import org.springframework.http.MediaType;
+
+import java.io.InputStream;
+
+public record BucketFile(String name, InputStream is, MediaType type, Long size) {
+}
